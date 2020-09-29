8524168472d42add2da50bf67b3a558446817e0d
Priyanka Patel <61297173+Priyankaa-patel@users.noreply.github.com>
2020-09-28 22:08:48 -0700

    Updates

---
+++ precache-manifest.9df6549c0b597cca7c13a585c9e8cdcb.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "758882b471ef43863a15c014cac2dd8f",
+    "url": "/PricelineReactHomePage/index.html"
+  },
+  {
+    "revision": "818c2f5afaabf4cb3652",
+    "url": "/PricelineReactHomePage/static/css/main.e2e55f34.chunk.css"
+  },
+  {
+    "revision": "95206d7e0a84dbdc4f4b",
+    "url": "/PricelineReactHomePage/static/js/2.54fbfed8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PricelineReactHomePage/static/js/2.54fbfed8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "818c2f5afaabf4cb3652",
+    "url": "/PricelineReactHomePage/static/js/main.f56494a4.chunk.js"
+  },
+  {
+    "revision": "9077dea35d23a67a3b20",
+    "url": "/PricelineReactHomePage/static/js/runtime-main.566f0c6e.js"
+  },
+  {
+    "revision": "481cf52cda3bb391a7cdf00cd15cf3cf",
+    "url": "/PricelineReactHomePage/static/media/CHICAGO.481cf52c.jpg"
+  },
+  {
+    "revision": "18394b1952823d9fff9e77e55b859a78",
+    "url": "/PricelineReactHomePage/static/media/HONOLULU.18394b19.jpg"
+  },
+  {
+    "revision": "4bb578b22a059ea321e4f2ac06d677c5",
+    "url": "/PricelineReactHomePage/static/media/LASVEGAS.4bb578b2.jpg"
+  },
+  {
+    "revision": "c182d0b22e09dea11266cf99212f1f6c",
+    "url": "/PricelineReactHomePage/static/media/LOSANGELES.c182d0b2.jpg"
+  },
+  {
+    "revision": "b5a88267270b7da30a83d3c95ab6000c",
+    "url": "/PricelineReactHomePage/static/media/MIAMIBEACH.b5a88267.jpg"
+  },
+  {
+    "revision": "6bcc47bbc538666d2ff882a2c2f1916f",
+    "url": "/PricelineReactHomePage/static/media/ORLANDO.6bcc47bb.jpg"
+  },
+  {
+    "revision": "b7a62314a7457c16679e35a00350eff4",
+    "url": "/PricelineReactHomePage/static/media/agoda.b7a62314.svg"
+  },
+  {
+    "revision": "9d28050f8dbfabfaef7246adea39daa7",
+    "url": "/PricelineReactHomePage/static/media/atlanta.9d28050f.webp"
+  },
+  {
+    "revision": "e7015bab7d9f01f7a44bf09e29386b13",
+    "url": "/PricelineReactHomePage/static/media/banner.e7015bab.jpg"
+  },
+  {
+    "revision": "fa1db478b2b7df645ec9d50955320d2b",
+    "url": "/PricelineReactHomePage/static/media/booking.fa1db478.svg"
+  },
+  {
+    "revision": "7a93d1c94cd1a89001f49ccc925c1899",
+    "url": "/PricelineReactHomePage/static/media/dallas.7a93d1c9.webp"
+  },
+  {
+    "revision": "1a706185600a9c01046eae3983901c8b",
+    "url": "/PricelineReactHomePage/static/media/denver.1a706185.webp"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/PricelineReactHomePage/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/PricelineReactHomePage/static/media/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/PricelineReactHomePage/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/PricelineReactHomePage/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/PricelineReactHomePage/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "78aa0e4e9cc3e4201cb35f54cc42e662",
+    "url": "/PricelineReactHomePage/static/media/hotel-collections.78aa0e4e.jpg"
+  },
+  {
+    "revision": "ad862e7ac0012a704eac7473fecf7a9e",
+    "url": "/PricelineReactHomePage/static/media/kayak.ad862e7a.svg"
+  },
+  {
+    "revision": "f65b020e66020ee5ce12776565e9256a",
+    "url": "/PricelineReactHomePage/static/media/los-angeles.f65b020e.webp"
+  },
+  {
+    "revision": "1df1239e96c8c0a0f0afdbdafdd99be2",
+    "url": "/PricelineReactHomePage/static/media/miami.1df1239e.webp"
+  },
+  {
+    "revision": "ef1bdb2b272342fc6cc412c1bbfe7d49",
+    "url": "/PricelineReactHomePage/static/media/new-york-city.ef1bdb2b.webp"
+  },
+  {
+    "revision": "83023546d9fb6bc56f8db526f4422a13",
+    "url": "/PricelineReactHomePage/static/media/opentable.83023546.svg"
+  },
+  {
+    "revision": "4da5bcd6d86a13638cc46c76e8b0cfe3",
+    "url": "/PricelineReactHomePage/static/media/orlando.4da5bcd6.webp"
+  },
+  {
+    "revision": "809559d8a824f61c29ceaac3ffa84b26",
+    "url": "/PricelineReactHomePage/static/media/priceline.809559d8.svg"
+  },
+  {
+    "revision": "e40ba03951ec6b0e8bc84fe0b08550e2",
+    "url": "/PricelineReactHomePage/static/media/rentalcars.e40ba039.svg"
+  },
+  {
+    "revision": "256b78c16553465cadacfd7b07c96b77",
+    "url": "/PricelineReactHomePage/static/media/san-diego.256b78c1.webp"
+  },
+  {
+    "revision": "5434a7b201e78878a68793f42eb2d3b1",
+    "url": "/PricelineReactHomePage/static/media/san-francisco.5434a7b2.webp"
+  },
+  {
+    "revision": "249b78e7cc013201c801166a570fdbe2",
+    "url": "/PricelineReactHomePage/static/media/seattle.249b78e7.webp"
+  },
+  {
+    "revision": "dcc2d77c665d5e29827d8a78a2093527",
+    "url": "/PricelineReactHomePage/static/media/spring.dcc2d77c.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.047f8d611b9a813e2730bc85caae53d9.js"
+  "/PricelineReactHomePage/precache-manifest.9df6549c0b597cca7c13a585c9e8cdcb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/PricelineReactHomePage/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
